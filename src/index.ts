102f436e28878a1bfaf72c16dd2cf1e7548713f1
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2020-10-08 02:25:23 +0300

    add apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,29 @@
 import { MikroORM } from "@mikro-orm/core";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  //   const post = orm.em.create(Post, {
-  //     title: "my first post",
-  //   });
-  //   await orm.em.persistAndFlush(post);
 
-  const posts = await orm.em.find(Post, {});
+  const app = express();
 
-  console.log(posts);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server started on localhost:4000");
+  });
 };
 
 main();
