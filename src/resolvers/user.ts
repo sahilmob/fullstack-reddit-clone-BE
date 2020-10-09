fabdffa06516bde2765d9a2f9e7c29542615638e
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2020-10-09 23:59:10 +0300

    create login query

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,13 @@
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from "type-graphql";
 import argon2 from "argon2";
 
 import { User } from "../entities";
@@ -10,6 +19,25 @@ class UsernamePasswordInput {
   username: string;
   @Field()
   password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+// object types are what could be assigned to the return value
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
 
 @Resolver()
@@ -26,5 +54,32 @@ export class UserResolver {
     });
     await em.persistAndFlush(user);
     return user;
+  }
+
+  @Query(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: ApolloContext
+  ): Promise<UserResponse> {
+    try {
+      const user = await em.findOne(User, { username: options.username });
+      if (!user) {
+        return {
+          errors: [{ field: "", message: "invalid username or password" }],
+        };
+      }
+
+      const passwordMatches = await argon2.verify(
+        user.password,
+        options.password
+      );
+      if (!passwordMatches)
+        return {
+          errors: [{ field: "", message: "invalid username or password" }],
+        };
+      return { user };
+    } catch (error) {
+      return error;
+    }
   }
 }
